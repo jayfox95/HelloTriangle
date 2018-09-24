bfc494e354b9d6e4ebf103c6c55c231fc61d7ef5
jayfox95 <31676058+jayfox95@users.noreply.github.com>
2018-09-23 23:26:26 -0500

    Update HelloTriangle.js

--- HelloTriangle/HelloTriangle.js
+++ HelloTriangle/HelloTriangle.js
@@ -8,31 +8,31 @@ var VSHADER_SOURCE = //this sets up a function that is called later whenm we ini
 
 //fragment shader
 var FSHADER_SOURCE = //this is a function that is called when we init shaders
-    'void main() {\n' +
+    'void main() {\n' + // calling the main function
     '   gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' + //setting the color for the triangle
     '}\n';
 
-function main() {
+function main() { //setting up the main function
     //retrieve canvas for the shape
-    var canvas = document.getElementById('webgl');
+    var canvas = document.getElementById('webgl'); //in the DOM, get the element (that we are naming 'canvas') that has the ID 'webgl'
 
     //gets the 3d rendering context foor webgl
     var gl = getWebGLContext(canvas); //creates a variable to get the context of our previous vcariable canvas
     if (!gl) { //if we cannot get the context then we return this error message
-        console.log('Failed to get the rendering context for webGL');
+        console.log('Failed to get the rendering context for webGL'); //display message when the rendering context cannot be retrieved
         return;
     }
 
     //here we check to see if our shaders initialized
-    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.log('Failed to initialize the shaders');
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) { //if the shaders cannot be initialized
+        console.log('Failed to initialize the shaders'); //display message when the program fails to initialize shaders
         return;
     }
 
     //write the positions of the vertices to our vertex shader
-    var n = initVertexBuffers(gl); 
-    if (n < 0) {
-        console.log('Failed to set the positions of the vertices');
+    var n = initVertexBuffers(gl); //n is the number of vertices
+    if (n < 0) { //if there are no vertices established
+        console.log('Failed to set the positions of the vertices'); //display message when the vertices cannot be found
         return;
     }
 
@@ -47,16 +47,16 @@ function main() {
 }
 
 function initVertexBuffers(gl) { //this is where we actually set the positions of the vertices
-    var vertices = new Float32Array([
+    var vertices = new Float32Array([ //typed array of float values
         0, 0.5, -0.5, -0.5, 0.5, -0.5 //these are the coordinates of the vertices stored in an artray for us
     ]);
 
     var n = 3; //this is the number of vertices and what is checkedf later
 
     //create a buffer object
-    var vertexBuffer = gl.createBuffer();
+    var vertexBuffer = gl.createBuffer(); //creates a buffer object called vertexBuffer
     if (!vertexBuffer) { //error for if the buffer object can't be created
-        console.log('Failed to create the buffer object');
+        console.log('Failed to create the buffer object'); //display message when buffer object creation fails
         return -1;
     }
 
@@ -66,9 +66,9 @@ function initVertexBuffers(gl) { //this is where we actually set the positions o
     //this writes data into the buffeer object
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position'); 
     if (a_Position < 0) { //lets us know if everything is working
-        console.log('Failed to get the storage location of a_Position');
+        console.log('Failed to get the storage location of a_Position'); //display message when the location of a_Position cannot be found
         return -1;
     }
 
@@ -79,7 +79,7 @@ function initVertexBuffers(gl) { //this is where we actually set the positions o
     gl.enableVertexAttribArray(a_Position);
 
 
-    return n; //this is what allows us to check
+    return n; //this is what allows us to check the number of vertices
 }
 
 
